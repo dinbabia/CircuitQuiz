80e279da6ea1470ec52db4487c926c0149c0a914
Din Laurence C. Babia <dinbabia@gmail.com>
2022-08-28 01:27:54 +0800

    update frontend form

--- frontend/home.py
+++ frontend/home.py
@@ -14,10 +14,10 @@ def check_form_input()-> None:
         messagebox.showerror(title="Basic Circuit Quiz | Incomplete Form", message="Please fill up the form completely.")
     else:
         answer = messagebox.askyesno('Basic Circuit Quiz | Complete Form', f"""Do you want to proceed and take the quiz with this information?\n
+\t{date["text"]}\n
 Student's Name:       {user_name_input.get()}
 Instructor's Name:    {instructors_name_input.get()}
-Year and Section:      {year_and_section_input.get()}
-{date["text"]}""")
+Year and Section:      {year_and_section_input.get()}""")
         if answer:
             win.destroy()
             # START EXAM
@@ -40,35 +40,34 @@ st = Style()
 st.configure('W.TButton', background='#345', foreground='green', font=('Arial', 14 ))
 
 # WELCOME NOTE
-welcome_note = Label(text="Fill up the form before starting the quiz.", font=("Times New Roman", 15, "bold"))
+welcome_note = Label(text="Fill up the form before starting the quiz.", font=("Times New Roman", 15, "bold"), background="white")
 welcome_note.grid(column=2, row=4, columnspan = 2, pady=(0, 30))
 
+# DATE
+date = Label(text=f"Date: {datetime.now().strftime('%B %d,%Y')}", font=("Courier", 12, "bold"), background="white")
+date.grid(column=2, row=5,  columnspan = 2,pady=(0, 20))
 
 # USER NAME
-user_name_label = Label(text="Student's Name:", font=("Courier", 12, "bold"))
-user_name_label.grid(column=2, row=5, sticky = "w")
+user_name_label = Label(text="Student's Name:", font=("Courier", 12, "bold"), background="white")
+user_name_label.grid(column=2, row=6, sticky = "w")
 
 user_name_input = Entry( width=30)
-user_name_input.grid(column=3, row=5, padx=(20, 0))
+user_name_input.grid(column=3, row=6, padx=(20, 0))
 
 
 # INSTRUCTOR'S NAME
-instructors_name_label = Label(text="Instructor's Name:", font=("Courier", 12, "bold"))
-instructors_name_label.grid(column=2, row=6 , pady=(20, 0), sticky = "w")
+instructors_name_label = Label(text="Instructor's Name:", font=("Courier", 12, "bold"), background="white")
+instructors_name_label.grid(column=2, row=7 , pady=(20, 0), sticky = "w")
 
 instructors_name_input = Entry(width=30)
-instructors_name_input.grid(column=3, row=6, padx=(20, 0), pady=(10, 0))
+instructors_name_input.grid(column=3, row=7, padx=(20, 0), pady=(10, 0))
 
 # YEAR AND SECTION
-year_and_section_label = Label(text="Year and Section:", font=("Courier", 12, "bold"))
-year_and_section_label.grid(column=2, row=7 , pady=(20, 0), sticky = "w")
+year_and_section_label = Label(text="Year and Section:", font=("Courier", 12, "bold"), background="white")
+year_and_section_label.grid(column=2, row=8 , pady=(20, 0), sticky = "w")
 
 year_and_section_input = Entry(width=30)
-year_and_section_input.grid(column=3, row=7, padx=(20, 0), pady=(10, 0))
-
-# DATE
-date = Label(text=f"Date: {datetime.now().strftime('%B %d,%Y')}", font=("Courier", 12, "bold"))
-date.grid(column=2, row=8, sticky = "w", pady=(20, 0))
+year_and_section_input.grid(column=3, row=8, padx=(20, 0), pady=(10, 0))
 
 # START BUTTON
 start_button = Button(win, text="Start", style="W.TButton", command=check_form_input)
